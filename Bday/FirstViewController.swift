3232af02bef3e8ecb427c9db20b8434ced0b4fe8
Vojtech Rinik <vojto@rinik.net>
2016-12-12 15:45:26 +0100

    Initial Commit

---
+++ Bday/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bday
+//
+//  Created by Vojtech Rinik on 12/12/2016.
+//  Copyright © 2016 Vojtech Rinik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
